72939fee2bd3441c9e9f2f29d5968e1e93836fcd
Renuka_VJ <renukavellingiri007@gmail.com>
2025-06-28 14:08:56 +0530

    aboutus Page Updated

--- JS/aboutus.js
+++ JS/aboutus.js
@@ -4,7 +4,7 @@ let interval=5000;
 valueDisplay.forEach((valueDisplay)=>{
 let startValue=0;
 let endValue=parseInt(valueDisplay.getAttribute("data-val"));
-// console.log(endValue);
+
 
 let duration=Math.floor(interval/endValue);
 let counter=setInterval(function()
